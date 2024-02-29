8a740ad4790ae3afbef38db8b8a46bf4da49eee9
KevinGomes <36061563+KevinGomes@users.noreply.github.com>
2024-02-29 02:18:34 -0500

    updated scripts

--- script.js
+++ script.js
@@ -5,20 +5,35 @@ window.onload=function(){
         // When the button is clicked, the function provided as the second argument will run.
 
         // Step 2: Get the comment from the textarea
+        const newUserName = document.getElementById("display-name").value;
         const newComment = document.getElementById("message").value;
 
 
-        // 
+        fetch("http://localhost:3000/create-message", {
+            method: "POST",
+            body: JSON.stringify({
+            username: newUserName,
+            message: newComment
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
 
-
-
-
-
-
-
-
-
-
+        fetch("http://localhost:3000/create-message", {
+            method: "POST",
+            body: JSON.stringify({
+            username: newUserName,
+            message: newComment
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
 
         // Step 3: Create a new paragraph element for the comment
         const commentContainer = document.getElementById("inner-display");
