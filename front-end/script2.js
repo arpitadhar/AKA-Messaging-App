271e0fdeb35fdb619ecf6bde1ce752bc8f67941a
Arpita Dhar <92829435+arpitadhar@users.noreply.github.com>
2024-03-12 13:00:44 -0400

    Create script2.js
    javascript for registration page

---
+++ front-end/script2.js
@@ -0,0 +1,66 @@
+window.onload=function(){
+
+document.getElementById("enter").addEventListener("click", function () {
+    // This line sets up an event listener for the click event on the "Post" button.
+    // When the button is clicked, the function provided as the second argument will run.
+    
+    // Step 2: Get the comment from the textarea
+    const newUserName = document.getElementById("user").value;
+    const newFirstName = document.getElementById("first_name").value; 
+    const newLastName = document.getElementById("last_name").value; 
+    const newPassword = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirm_password").value; 
+    const newEmail = document.getElementById("email").value; 
+    
+    if (newPassword == '' || confirmPassword == ''){
+        alert("Please enter a password"); 
+        return false; 
+    }
+    
+    else if (newPassword != confirmPassword){
+       alert("Passwords do not match"); 
+       return false; 
+    }
+    else{
+        alert("Account created!"); 
+    }
+
+    
+    
+
+    fetch("http://localhost:3000/create-users", {
+        method: "POST",
+        body: JSON.stringify({
+        username: newUserName,
+        first_name: newFirstName, 
+        last_name: newLastName, 
+        password: newPassword,
+        email: newEmail,
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+
+    document.getElementById("user").value = "";
+    document.getElementById("first_name").value=""; 
+    document.getElementById("last_name").value=""; 
+    document.getElementById("password").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("confirm_password").value = ""; 
+    
+
+    // // Step 3: Create a new paragraph element for the comment
+    // const commentContainer = document.getElementById("inner-display");
+    // const commentElement = document.createElement("p");
+    // commentElement.innerText = new;
+
+    // // Step 4: Append the comment to the comment container
+    // commentContainer.appendChild(commentElement);
+
+    // // Clear the textarea after posting the comment
+    // document.getElementById("message").value = "";
+});
+}
