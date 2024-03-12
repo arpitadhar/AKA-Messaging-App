0fb6cf698b7d6c503964ad86c03283e518454491
Arpita Dhar <92829435+arpitadhar@users.noreply.github.com>
2024-03-12 15:39:34 -0400

    Update models.rs

--- src/models.rs
+++ src/models.rs
@@ -1,10 +1,24 @@
 use diesel::prelude::*;
+use serde::{Deserialize, Serialize};
 
-#[derive(Queryable, Selectable)]
+#[derive(Queryable, Selectable, Serialize, Debug)]
 #[diesel(table_name = crate::schema::messages)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
+
 pub struct Messages {
     pub id: i32,
     pub user_id: String,
     pub message: String,
+}
+use diesel::{Queryable, Selectable};
+#[derive(Queryable, Selectable, Serialize, Debug)]
+#[diesel(table_name = crate::schema::users)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Users {
+    pub id: i32,
+    pub username: String,
+    pub first_name: String, 
+    pub last_name: String, 
+    pub email: String,
+    pub password: String,
 }
